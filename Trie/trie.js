930a30200c9afdf896b7f12407c5192a655d26bb
Riyazo4 <mohammedriyazriyaz04@gmail.com>
2023-09-12 00:06:36 +0530

    trie

---
+++ Trie/trie.js
@@ -0,0 +1,57 @@
+class trieNode {
+  constructor() {
+    this.child = new Map();
+    this.endOfWord = false;
+  }
+  markAsLeaf() {
+    this.endOfWord = true;
+  }
+  unMarkAsLeaf() {
+    this.endOfWord = false;
+  }
+}
+class Trie {
+  constructor() {
+    this.root = new trieNode();
+  }
+
+  insert(word) {
+    let curr = this.root;
+    for (let i = 0; i < word.length; i++) {
+      let ch = word[i];
+      let node = curr.child.get(ch);
+      if (!node) {
+        node = new trieNode();
+        curr.child.set(ch, node);
+      }
+      curr = node;
+    }
+    curr.markAsLeaf();
+  }
+  search(word) {
+    let curr = this.root;
+    for (let i = 0; i < word.length; i++) {
+      let ch = word[i];
+      let node = curr.child.get(ch);
+      if (!node) {
+        return false;
+      }
+      curr = node
+    }
+    return curr.endOfWord
+  }
+  display(curr=this.root,prefix=""){
+    if(curr.endOfWord){
+        console.log(prefix);
+    }
+    for(let [ch,node] of curr.child){
+        this.display(node,prefix+ch)
+    }
+  }
+}
+let tr = new Trie();
+tr.insert("Hello");
+tr.insert("Hei");
+// console.log(tr.search("Hei"));
+tr.display();
+console.log(tr);
